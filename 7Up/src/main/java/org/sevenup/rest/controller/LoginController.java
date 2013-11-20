ad4e188ad73f81527d99b5f875a12fe228e8f6a5
Steven.xu <nonumber1989@gmail.com>
2013-11-20 16:08:08 +0800

    steven

--- 7Up/src/main/java/org/sevenup/rest/controller/LoginController.java
+++ 7Up/src/main/java/org/sevenup/rest/controller/LoginController.java
@@ -1,9 +1,13 @@
 package org.sevenup.rest.controller;
 
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /**
@@ -12,11 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
+@Produces(MediaType.APPLICATION_JSON)
 public class LoginController {
     private static Logger LOG = LoggerFactory.getLogger(LoginController.class);
     @RequestMapping("7Up/login")
-    public void seven(){
+    public String login(){
     	System.out.println("seven");
     	System.out.println("seven");
+    	return "welcome";
     }
 }
