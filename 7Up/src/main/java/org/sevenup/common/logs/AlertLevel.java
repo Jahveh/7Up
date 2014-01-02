2224dca56feb1f9f148d1343de78e7b13ab41354
steven.xu <steven.xu@LCHN06NU344CCJH.cardinalhealth.net>
2014-01-02 14:57:40 +0800

    Steven Log and Exception design

---
+++ 7Up/src/main/java/org/sevenup/common/logs/AlertLevel.java
@@ -0,0 +1,15 @@
+package org.sevenup.common.logs;
+
+
+/**
+ * This lists the alert levels allowed by the Cardinal Alerter
+ *
+ * @author Jason Cardillo
+ * @version $Revision: 0.1$
+ */
+public final class AlertLevel {
+    public final static String WARNING = "Warning";
+    public final static String CRITICAL ="Critical";
+    
+    private AlertLevel(){}
+}
---
+++ 7Up/src/main/java/org/sevenup/common/logs/AlertType.java
@@ -0,0 +1,17 @@
+package org.sevenup.common.logs;
+
+
+/**
+ * This lists the alert types allowed by the Cardinal Alerter
+ *
+ * @author Jason Cardillo
+ * @version $Revision: 0.1$
+ */
+public final class AlertType {
+    //	 Constant Variables
+    public final static String SECURITY = "Security";
+    public final static String SYSTEM = "System";
+    public final static String BUSINESS = "Business";
+    
+    private AlertType(){}
+}
